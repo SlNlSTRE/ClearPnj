c6f4b848050ecb9b63fc965ba7fe3309b696e8ae
SlNlSTRE <benchimol.sarah67@gmail.com>
2021-01-27 22:51:18 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,17 @@
+local config = {
+    pedFrequency = 1.0,
+    trafficFrequency = 1.0,
+}
+
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        SetPedDensityMultiplierThisFrame(config.pedFrequency) 
+        SetScenarioPedDensityMultiplierThisFrame(config.pedFrequency, config.pedFrequency) 
+        -------------------------------
+        SetRandomVehicleDensityMultiplierThisFrame(config.trafficFrequency) 
+        SetParkedVehicleDensityMultiplierThisFrame(config.trafficFrequency) 
+        SetVehicleDensityMultiplierThisFrame(config.trafficFrequency) 
+    end 
+end)
